a4f91590cf41d92dca367e5515ce4f9fd62e287d
Mathieu Méa <mathieu.mea@gmail.com>
2015-07-13 17:00:59 -0400

    Minor improvements...

--- src/org/mtransit/parser/ca_kamloops_transit_system_bus/KamloopsTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_kamloops_transit_system_bus/KamloopsTransitSystemBusAgencyTools.java
@@ -35,7 +35,7 @@ public class KamloopsTransitSystemBusAgencyTools extends DefaultAgencyTools {
 
 	@Override
 	public void start(String[] args) {
-		System.out.printf("\nGenerating Kamloops Transit System bus data...\n");
+		System.out.printf("\nGenerating Kamloops Transit System bus data...");
 		long start = System.currentTimeMillis();
 		this.serviceIds = extractUsefulServiceIds(args, this);
 		super.start(args);
