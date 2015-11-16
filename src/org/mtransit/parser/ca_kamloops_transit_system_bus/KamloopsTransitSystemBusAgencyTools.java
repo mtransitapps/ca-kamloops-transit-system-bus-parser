23b2dac53af7026d5082b61270789149b37dd29a
Mathieu Méa <mathieu.mea@gmail.com>
2015-11-16 11:15:34 -0500

    Better filtering of service IDs for multi-agencies GTFS files.

--- src/org/mtransit/parser/ca_kamloops_transit_system_bus/KamloopsTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_kamloops_transit_system_bus/KamloopsTransitSystemBusAgencyTools.java
@@ -37,7 +37,7 @@ public class KamloopsTransitSystemBusAgencyTools extends DefaultAgencyTools {
 	public void start(String[] args) {
 		System.out.printf("\nGenerating Kamloops Transit System bus data...");
 		long start = System.currentTimeMillis();
-		this.serviceIds = extractUsefulServiceIds(args, this);
+		this.serviceIds = extractUsefulServiceIds(args, this, true);
 		super.start(args);
 		System.out.printf("\nGenerating Kamloops Transit System bus data... DONE in %s.\n", Utils.getPrettyDuration(System.currentTimeMillis() - start));
 	}
