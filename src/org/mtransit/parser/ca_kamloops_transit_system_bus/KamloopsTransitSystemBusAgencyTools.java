45ce98767930c8b1f6647d61fcec09a5f9a6489f
Mathieu Méa <mathieu.mea@gmail.com>
2018-09-17 09:38:34 -0400

    Compatibility with latest update

--- src/org/mtransit/parser/ca_kamloops_transit_system_bus/KamloopsTransitSystemBusAgencyTools.java
+++ src/org/mtransit/parser/ca_kamloops_transit_system_bus/KamloopsTransitSystemBusAgencyTools.java
@@ -343,10 +343,12 @@ public class KamloopsTransitSystemBusAgencyTools extends DefaultAgencyTools {
 		return CleanUtils.cleanLabel(tripHeadsign);
 	}
 
+	private static final Pattern STARTS_WITH_IMPL = Pattern.compile("(^(\\(\\-IMPL\\-\\)))", Pattern.CASE_INSENSITIVE);
 	private static final Pattern STARTS_WITH_BOUND = Pattern.compile("(^(east|west|north|south)bound)", Pattern.CASE_INSENSITIVE);
 
 	@Override
 	public String cleanStopName(String gStopName) {
+		gStopName = STARTS_WITH_IMPL.matcher(gStopName).replaceAll(StringUtils.EMPTY);
 		gStopName = STARTS_WITH_BOUND.matcher(gStopName).replaceAll(StringUtils.EMPTY);
 		gStopName = CleanUtils.CLEAN_AT.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
 		gStopName = EXCHANGE.matcher(gStopName).replaceAll(EXCHANGE_REPLACEMENT);
